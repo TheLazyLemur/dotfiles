f8453ef66b5540bd09b311e14f896811ed7bb121
danielrousseau <drousseau@luno.com>
2024-10-08 20:13:53 +0200

    Esc to exit insert mode in terminals

--- private_dot_config/MyVim/lua/core/keymaps.lua
+++ private_dot_config/MyVim/lua/core/keymaps.lua
@@ -31,3 +31,5 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
