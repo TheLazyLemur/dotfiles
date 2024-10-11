29354be38b9f45f9520b669254e2d1e0c1b33f98
danielrousseau <drousseau@luno.com>
2024-10-11 11:57:52 +0200

    Using mini.nvim instead of mini.deps

--- private_dot_config/MyVim/lua/core/mini.lua
+++ private_dot_config/MyVim/lua/core/mini.lua
@@ -1,36 +1,30 @@
 local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.deps'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.deps`" | redraw')
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
   local clone_cmd = {
     'git', 'clone', '--filter=blob:none',
-    'https://github.com/echasnovski/mini.deps', mini_path
+    'https://github.com/echasnovski/mini.nvim', mini_path
   }
   vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.deps | helptags ALL')
-  vim.cmd('echo "Installed `mini.deps`" | redraw')
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
 require('mini.deps').setup({ path = { package = path_package } })
 
-MiniDeps.add("echasnovski/mini.statusline")
 require("mini.statusline").setup()
 
-MiniDeps.add("echasnovski/mini.basics")
 require("mini.basics").setup()
 
-MiniDeps.add("echasnovski/mini.icons")
 require("mini.icons").setup()
 require("mini.icons").mock_nvim_web_devicons()
 
-MiniDeps.add("echasnovski/mini.extra")
 require("mini.extra").setup()
 
-MiniDeps.add("echasnovski/mini.pick")
 require("mini.pick").setup()
 vim.ui.select = require("mini.pick").ui_select
 
-MiniDeps.add("echasnovski/mini.hipatterns")
 local hipatterns = require('mini.hipatterns')
 hipatterns.setup({
   highlighters = {
@@ -42,5 +36,4 @@ hipatterns.setup({
   },
 })
 
-MiniDeps.add("echasnovski/mini.jump2d")
 require('mini.jump2d').setup()
