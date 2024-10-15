6b13c1728d06f999d5cf33af51ac48ccf8f9b754
Dan Rousseau <danrousseau@protonmail.com>
2024-10-15 13:26:10 +0200

    Autoformat on save

--- private_dot_config/MyVim/lua/core/lsp.lua
+++ private_dot_config/MyVim/lua/core/lsp.lua
@@ -65,4 +65,13 @@ later(function()
             use_nvim_cmp_as_default = true,
         },
     })
+
+    local augroup = vim.api.nvim_create_augroup("MyVim-FT-Format", { clear = true })
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = "*",
+        callback = function()
+            vim.lsp.buf.format({ async = true })
+        end,
+        group = augroup,
+    })
 end)
