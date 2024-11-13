44e639ebdb02fd16405f2df31d02950c83837e5c
danielrousseau <drousseau@luno.com>
2024-11-13 15:52:56 +0200

    Added hammerspoon config

---
+++ dot_hammerspoon/aerospace.lua
@@ -0,0 +1,119 @@
+local AEROSPACE = "/opt/homebrew/bin/aerospace"
+
+local menuBarItem = hs.menubar.new()
+
+local M = {
+    last_dir = "horizontal",
+    layer = "one",
+}
+
+local function setTitle(val)
+    menuBarItem:setTitle(val)
+end
+
+local function splitByNewline(text)
+    local lines = {}
+    for line in string.gmatch(text, "[^\r\n]+") do
+        table.insert(lines, line)
+    end
+    return lines
+end
+
+local function aerospaceExec(cmd)
+    os.execute("nohup " .. AEROSPACE .. " " .. cmd .. " &")
+end
+
+M.toggle_layer = function()
+    if M.layer == "one" then
+        M.layer = "two"
+    else
+        M.layer = "one"
+    end
+    M.setTitle(M.layer)
+end
+
+M.setTitle = function(val)
+    setTitle(val)
+end
+
+M.focus_left = function()
+    aerospaceExec("focus left")
+end
+
+M.focus_right = function()
+    aerospaceExec("focus right")
+end
+
+M.focus_up = function()
+    aerospaceExec("focus up")
+end
+
+M.focus_down = function()
+    aerospaceExec("focus down")
+end
+
+M.swap_monitor = function()
+    aerospaceExec("move-workspace-to-monitor prev")
+end
+
+M.open_terminal = function()
+    local output, _, _, _ = hs.execute(AEROSPACE .. " list-windows --workspace visible")
+    local lines = splitByNewline(output)
+    if #lines == 1 then
+        aerospaceExec("split horizontal")
+        M.last_dir = "horizontal"
+    end
+
+    if #lines > 1 then
+        if M.last_dir == "horizontal" then
+            aerospaceExec("split vertical")
+            M.last_dir = "vertical"
+        else
+            aerospaceExec("split horizontal")
+            M.last_dir = "horizontal"
+        end
+    end
+
+
+    os.execute("nohup " .. "/opt/homebrew/bin/alacritty" .. " &")
+end
+
+M.move_to_workspace = function(i)
+    if M.layer == "two" then
+        aerospaceExec("workspace " .. tostring(i + 10))
+    else
+        aerospaceExec("workspace " .. tostring(i))
+    end
+end
+
+M.move_node_to_workspace = function(i)
+    if M.layer == "two" then
+        aerospaceExec("move-node-to-workspace " .. tostring(i + 10))
+    else
+        aerospaceExec("move-node-to-workspace " .. tostring(i))
+    end
+end
+
+M.move_left = function()
+    aerospaceExec("move left")
+end
+
+M.move_right = function()
+    aerospaceExec("move right")
+end
+
+M.move_up = function()
+    aerospaceExec("move up")
+end
+
+M.move_down = function()
+    aerospaceExec("move down")
+end
+
+M.setup = function()
+    aerospaceExec("split horizontal")
+    setTitle(M.layer)
+end
+
+
+return M
---
+++ dot_hammerspoon/init.lua
@@ -0,0 +1,35 @@
+local aerospace = require("aerospace")
+
+local modA = "alt"
+local shiftKey = "shift"
+local left = "h"
+local down = "j"
+local up = "k"
+local right = "l"
+
+aerospace.setup()
+
+hs.hotkey.bind({ modA, shiftKey }, "tab", aerospace.swap_monitor)
+
+hs.hotkey.bind({ modA }, left, aerospace.focus_left)
+hs.hotkey.bind({ modA }, down, aerospace.focus_down)
+hs.hotkey.bind({ modA }, up, aerospace.focus_up)
+hs.hotkey.bind({ modA }, right, aerospace.focus_right)
+hs.hotkey.bind({ modA }, "v", aerospace.toggle_layer)
+
+for i = 1, 9 do
+    hs.hotkey.bind({ modA }, tostring(i), function()
+        aerospace.move_to_workspace(i)
+    end)
+
+    hs.hotkey.bind({ modA, shiftKey }, tostring(i), function()
+        aerospace.move_node_to_workspace(i)
+    end)
+end
+
+hs.hotkey.bind({ modA, shiftKey }, left, aerospace.move_left)
+hs.hotkey.bind({ modA, shiftKey }, down, aerospace.move_down)
+hs.hotkey.bind({ modA, shiftKey }, up, aerospace.move_up)
+hs.hotkey.bind({ modA, shiftKey }, right, aerospace.move_right)
+
+hs.hotkey.bind({ modA }, "return", aerospace.open_terminal)
