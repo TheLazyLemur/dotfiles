a514f0d50da0ecc57d7aebf4f2a66be252408ea5
Dan Rousseau <danielr@bash.com>
2024-12-11 22:51:22 +0200

    Remove built in compile module

--- private_dot_config/MyVim/lua/builtin/keymaps.lua
+++ private_dot_config/MyVim/lua/builtin/keymaps.lua
@@ -1,5 +1,4 @@
 local termmy = require("builtin.termmy")
-local compile = require("builtin.compile")
 local spear = require("builtin.spear")
 
 vim.keymap.set({ "n", "t" }, "<leader>tf", function() termmy.toggle(nil) end)
@@ -7,8 +6,6 @@ vim.keymap.set({ "n", "t" }, "<leader>tff", function() termmy.toggle(nil, "hello
 
 vim.keymap.set({ "n", "t" }, "<leader>tS", function() termmy.toggle(true, vim.fn.expand('%:p:h') .. '/') end)
 vim.keymap.set({ "n", "t" }, "<leader>tF", function() termmy.toggle(false, vim.fn.expand('%:p:h') .. '/') end)
-
-vim.keymap.set('n', '<leader>cc', compile.compile, { desc = '[E]rror [C]ompile' })
 
 vim.keymap.set("n", "<leader>mm", spear.throw)
 vim.keymap.set("n", "<leader>ms", spear.pick)
