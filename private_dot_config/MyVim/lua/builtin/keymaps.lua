7dab6c0b767c2af7c109581c48ac3e201a2b565e
Dan Rousseau <danielr@bash.com>
2024-12-13 09:17:49 +0200

    Moved tui wrapper to builtins

--- private_dot_config/MyVim/lua/builtin/keymaps.lua
+++ private_dot_config/MyVim/lua/builtin/keymaps.lua
@@ -1,5 +1,6 @@
 local termmy = require("builtin.termmy")
 local spear = require("builtin.spear")
+local wrapper = require("builtin.tuiwrapper")
 
 vim.keymap.set({ "n", "t" }, "<leader>tf", function() termmy.toggle(nil) end)
 vim.keymap.set({ "n", "t" }, "<leader>tff", function() termmy.toggle(nil, "hello") end)
@@ -10,3 +11,6 @@ vim.keymap.set({ "n", "t" }, "<leader>tF", function() termmy.toggle(vim.fn.expan
 vim.keymap.set("n", "<leader>mm", spear.throw)
 vim.keymap.set("n", "<leader>ms", spear.pick)
 vim.keymap.set("n", "<leader>md", function() spear.pick("delete") end)
+
+vim.keymap.set("n", "<leader>g", function() wrapper.interactive_command_wrapper("lazygit") end)
+vim.keymap.set("n", "<leader>d", function() wrapper.interactive_command_wrapper("lazydocker") end)
---
+++ private_dot_config/MyVim/lua/builtin/tuiwrapper.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+local create_temp_buf = function()
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf })
+    vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
+    return buf
+end
+
+local create_centered_win = function(buf)
+    local height = math.ceil(vim.o.lines * 0.8)
+    local width = math.ceil(vim.o.columns * 0.8)
+    local win = vim.api.nvim_open_win(buf, true, {
+        relative = "editor",
+        width = width,
+        height = height,
+        row = math.ceil((vim.o.lines - height) / 2),
+        col = math.ceil((vim.o.columns - width) / 2),
+        style = "minimal",
+        border = "rounded",
+    })
+
+    return win
+end
+
+M.interactive_command_wrapper = function(cmd)
+    local buf = create_temp_buf()
+    local win = create_centered_win(buf)
+    vim.api.nvim_set_current_win(win)
+    vim.fn.termopen(cmd, {
+        on_exit = function()
+            pcall(vim.api.nvim_win_close, win, true)
+        end
+    })
+end
+
+return M
