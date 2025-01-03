3e6ffd7240e6e269096d01c293035a673cd512d6
Dan Rousseau <danielr@bash.com>
2025-01-03 10:19:51 +0200

    Removed unused keymap

--- private_dot_config/MyVim/init.lua
+++ private_dot_config/MyVim/init.lua
@@ -1,9 +1,2 @@
 require("core")
 require("plugins")
-
-vim.keymap.set("n", "<leader>ss", function()
-    local current_win = vim.api.nvim_get_current_win()
-    vim.cmd('wincmd J')
-    vim.cmd('split')
-    vim.api.nvim_set_current_win(current_win)
-end)
