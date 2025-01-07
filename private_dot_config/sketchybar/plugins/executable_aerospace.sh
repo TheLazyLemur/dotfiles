09465a936454817203557640d7c85604f4392699
Dan Rousseau <danrousseau@protonmail.com>
2025-01-07 23:38:42 +0200

    Sketchybar config

---
+++ private_dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# make sure it's executable with:
+# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+
+if [ "$1" = "$(aerospace list-workspaces --focused)" ]; then
+    sketchybar --set $NAME background.drawing=on label.font="ComicShannsMono Nerd Font:Bold:22" \
+        label.color=0xffcdd6f4 \
+        icon.font="ComicShannsMono Nerd Font:Bold:22" \
+        label.color=0xffd7827e \
+        icon.color=0xffd7827e \
+        icon=" "
+else
+    sketchybar --set $NAME background.drawing=off label.font="ComicShannsMono Nerd Font:Bold:18" \
+        label.color=0x55cdd6f4 \
+        icon.font="ComicShannsMono Nerd Font:Bold:18" \
+        label.color=0xffcdd6f4 \
+        icon.color=0xffcdd6f4 \
+        icon=" "
+fi
