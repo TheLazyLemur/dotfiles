86843a9712712af27bafcacf974bfab66b8a29ce
Dan Rousseau <danrousseau@protonmail.com>
2025-01-12 14:05:07 +0200

    sketchybar: Change active worksapce indicatior colo

--- private_dot_config/sketchybar/plugins/executable_aerospace.sh
+++ private_dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -6,18 +6,17 @@
 if [ "$1" = "$(aerospace list-workspaces --focused)" ]; then
     sketchybar --set $NAME background.drawing=on \
         label.font="ComicShannsMono Nerd Font:Bold:22" \
-        label.color=0xffcdd6f4 \
         icon.font="ComicShannsMono Nerd Font:Bold:22" \
-        label.color=0xffe0def4 \
-        icon.color=0xffe0def4 \
-        background.border_color=0xffe0def4 \
+        label.color=0xff9ccfd8 \
+        icon.color=0xff9ccfd8 \
+        background.border_color=0xff9ccfd8 \
         icon=" "
 else
     sketchybar --set $NAME background.drawing=off \
         label.font="ComicShannsMono Nerd Font:Bold:18" \
         label.color=0x55cdd6f4 \
         icon.font="ComicShannsMono Nerd Font:Bold:18" \
-        label.color=0xffcdd6f4 \
-        icon.color=0xffcdd6f4 \
+        label.color=0xff6e6a86 \
+        icon.color=0xff6e6a86 \
         icon=" "
 fi
