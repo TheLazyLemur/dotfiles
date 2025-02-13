fca4f2823276f899046b34ebbf1fc74b6adc458d
Dan Rousseau <danrousseau@protonmail.com>
2025-02-13 08:01:44 +0200

    No need to install neovim as root

--- bin/executable_install_nvim.sh
+++ bin/executable_install_nvim.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-mkdir -p $HOME/src
-sudo rm -rf $HOME/src/neovim
-git clone https://github.com/neovim/neovim.git $HOME/src/neovim
-
 brew install ninja cmake gettext curl
+
+mkdir -p $HOME/src
+
+rm -rf $HOME/src/neovim
+git clone https://github.com/neovim/neovim.git $HOME/src/neovim
 
 cd $HOME/src/neovim
 
-sudo make install
+make CMAKE_INSTALL_PREFIX=$HOME/local/nvim install
+
+if ! grep -Fxq "export PATH=\$HOME/local/nvim/bin:\$PATH" ~/.zshrc; then
+    echo "export PATH=\$HOME/local/nvim/bin:\$PATH" >> ~/.zshrc
+    echo "Line added to .zshrc"
+else
+    echo "Line already exists in .zshrc"
+fi
