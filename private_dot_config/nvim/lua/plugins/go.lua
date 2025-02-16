25159ce13a33b606c3714bcfca657578ff9f996f
Dan Rousseau <danrousseau@protonmail.com>
2025-02-16 15:12:16 +0200

    Small chages

--- private_dot_config/nvim/lua/plugins/go.lua
+++ private_dot_config/nvim/lua/plugins/go.lua
@@ -6,7 +6,17 @@ return {
 		"catgoose/templ-goto-definition",
 	},
 	config = function()
-		require("go").setup({})
+		local has_run = false
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "go",
+			callback = function()
+				if has_run then
+					return
+				end
+				require("go").setup({})
+				has_run = true
+			end,
+		})
 		require("templ-goto-definition").setup()
 	end,
 }
--- private_dot_config/nvim/plugin/colorschemes.lua
+++ private_dot_config/nvim/plugin/colorschemes.lua
@@ -1,25 +1,27 @@
 vim.opt.background = "dark"
 
 MiniDeps.add({
-    name = "catppuccin",
-    source = "catppuccin/nvim",
+	name = "catppuccin",
+	source = "catppuccin/nvim",
 })
 require("catppuccin").setup({})
 
 MiniDeps.add({
-    name = "rose-pine",
-    source = "rose-pine/neovim",
+	name = "rose-pine",
+	source = "rose-pine/neovim",
 })
 require("rose-pine").setup({})
 
 MiniDeps.add({
-    name = "oxocarbon",
-    source = "nyoom-engineering/oxocarbon.nvim",
+	name = "oxocarbon",
+	source = "nyoom-engineering/oxocarbon.nvim",
 })
 
 MiniDeps.add({
-    name = "vscode",
-    source = "Mofiqul/vscode.nvim",
+	name = "vscode",
+	source = "Mofiqul/vscode.nvim",
 })
 
-vim.cmd.colorscheme("rose-pine")
+vim.cmd.colorscheme("oxocarbon")
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ffffff", bg = "None" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff", bg = "None" })
