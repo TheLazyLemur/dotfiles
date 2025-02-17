dc26db5d654eb7201b5f16ab4aa0a5f683c01225
Dan Rousseau <danrousseau@protonmail.com>
2025-02-17 21:33:11 +0200

    Moving away from lsp config
    Updaed plugins

--- private_dot_config/nvim/lua/core/init.lua
+++ private_dot_config/nvim/lua/core/init.lua
@@ -1,3 +1,4 @@
 require("core.options")
+require("core.lsp")
 require("core.keymaps")
 require("core.autocommands")
--- private_dot_config/nvim/lua/plugins/blink.lua
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -3,7 +3,6 @@ return {
 	dependencies = {
 		"rafamadriz/friendly-snippets",
 		"williamboman/mason.nvim",
-		"neovim/nvim-lspconfig",
 	},
 	build = "cargo build --release",
 	config = function()
@@ -29,53 +28,6 @@ return {
 			appearance = {
 				use_nvim_cmp_as_default = true,
 			},
-		})
-
-		local lspconfig = require("lspconfig")
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		local servers = { "gopls", "templ", "lua_ls", "zls", "clangd", "ts_ls", "volar", "svelte", "zls", "htmx" }
-		for _, server in ipairs(servers) do
-			if server == "gopls" then
-				require("lspconfig").gopls.setup({
-					capabilities = capabilities,
-					flags = {
-						debounce_text_changes = 150,
-					},
-					settings = {
-						gopls = {
-							hints = {
-								rangeVariableTypes = true,
-								parameterNames = true,
-								constantValues = true,
-								assignVariableTypes = true,
-								compositeLiteralFields = true,
-								compositeLiteralTypes = true,
-								functionTypeParameters = true,
-							},
-						},
-					},
-				})
-				vim.lsp.inlay_hint.enable()
-			else
-				lspconfig[server].setup({
-					flags = {
-						debounce_text_changes = 150,
-					},
-					capabilities = capabilities,
-				})
-			end
-		end
-
-		local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-		function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-			opts = opts or {}
-			opts.border = "rounded"
-			opts.width = math.floor(math.min(math.max(vim.o.columns * 0.4, 20), 80))
-			return orig_util_open_floating_preview(contents, syntax, opts, ...)
-		end
-
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-			border = "rounded",
 		})
 	end,
 }
