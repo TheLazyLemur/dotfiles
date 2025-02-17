854a290a01032925f78367f3ea4e2ab823895438
Dan Rousseau <danrousseau@protonmail.com>
2025-02-17 09:00:00 +0200

    Updated plugins and some highlight changes

--- private_dot_config/nvim/plugin/colorschemes.lua
+++ private_dot_config/nvim/plugin/colorschemes.lua
@@ -25,3 +25,4 @@ MiniDeps.add({
 vim.cmd.colorscheme("oxocarbon")
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ffffff", bg = "None" })
 vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff", bg = "None" })
+vim.cmd("highlight LspInlayHint guifg=#6C7C3C")
