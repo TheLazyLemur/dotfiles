c2ced45dea0788467e40ba689b9b56b4807e7bbf
Dan Rousseau <danielr@bash.com>
2025-04-29 13:21:40 +0200

    refactor(util): remove unused snacks_vertical function

--- private_dot_config/nvim/lua/util.lua
+++ private_dot_config/nvim/lua/util.lua
@@ -1,66 +1,47 @@
 local M = {}
 
 function M.lua_ls_on_init(client)
-	local path = vim.tbl_get(client, "workspace_folders", 1, "name")
-	if not path then
-		return
-	end
-	client.settings = vim.tbl_deep_extend("force", client.settings, {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME,
-				},
-			},
-		},
-	})
+    local path = vim.tbl_get(client, "workspace_folders", 1, "name")
+    if not path then
+        return
+    end
+    client.settings = vim.tbl_deep_extend("force", client.settings, {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                },
+            },
+        },
+    })
 end
 
 function M.snacks_vscode_bordered()
-	return {
-		preview = false,
-		layout = {
-			backdrop = false,
-			row = 2,
-			width = 0.4,
-			min_width = 80,
-			height = 0.4,
-			border = "none",
-			box = "vertical",
-			{
-				win = "input",
-				height = 1,
-				border = "rounded",
-				title = "{title} {live} {flags}",
-				title_pos = "center",
-			},
-			{ win = "list", border = "rounded" },
-			{ win = "preview", title = "{preview}", border = "rounded" },
-		},
-	}
-end
-
-function M.snacks_vertical()
-	return {
-		layout = {
-			backdrop = false,
-			width = 0.7,
-			min_width = 80,
-			height = 0.8,
-			min_height = 30,
-			box = "vertical",
-			border = "rounded",
-			title = "{title} {live} {flags}",
-			title_pos = "center",
-			{ win = "input", height = 1, border = "bottom" },
-			{ win = "list", border = "none", height = 0.3 },
-			{ win = "preview", title = "{preview}", height = 0.5, border = "top" },
-		},
-	}
+    return {
+        preview = false,
+        layout = {
+            backdrop = false,
+            row = 2,
+            width = 0.4,
+            min_width = 80,
+            height = 0.4,
+            border = "none",
+            box = "vertical",
+            {
+                win = "input",
+                height = 1,
+                border = "rounded",
+                title = "{title} {live} {flags}",
+                title_pos = "center",
+            },
+            { win = "list", border = "rounded" },
+            { win = "preview", title = "{preview}", border = "rounded" },
+        },
+    }
 end
 
 return M
